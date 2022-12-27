161cb3e07a6dba1499ac23f1a0ee9cb66cad4a31
arad1367 <pejman.ebrahimi77@gmail.com>
2022-12-27 12:58:21 +0100

    personal website first commit

---
+++ server.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request
+import smtplib
+from email.message import EmailMessage
+import os
+from pymongo import MongoClient
+
+app = Flask(__name__)
+
+cluster_address = os.environ['CLUSTER']
+cluster = MongoClient(cluster_address)
+db = cluster['personalSiteDB']
+collection = db['customers']
+
+@app.route('/', methods=['GET','POST'])
+def home_page():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        message = request.form['message']
+
+        customer = {
+            "Name": name,
+            "Email": email,
+            "Message": message
+        }
+
+        collection.insert_one(customer)
+
+        msg = EmailMessage()
+        msg.set_content(message)
+        owner_email = os.environ['OWNEREMAIL']
+        owner_pass = os.environ['OWNERPASS']
+        msg['Subject'] = 'New message from Personal website'
+        msg['From'] = f"{email}"
+        msg['To'] = owner_email
+        # Send the message via our own SMTP server.
+        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        server.login(owner_email, owner_pass)
+        server.send_message(msg)
+        server.quit()
+        return render_template('index.html', msg_sent=True, name=name)
+    return render_template('index.html', msg_sent=False)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ test.py
@@ -0,0 +1,5 @@
+from server import app
+
+def test1():
+    response = app.test_client().get('/')
+    assert response.status_code == 200
