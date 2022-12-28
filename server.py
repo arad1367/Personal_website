1660f8f30f5c165c84bbe595a346f2e48fb007bc
arad1367 <pejman.ebrahimi77@gmail.com>
2022-12-28 10:58:28 +0100

    Update to PostgreSQL

--- server.py
+++ server.py
@@ -8,7 +8,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 ##Connect to Database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///personalWeb.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
