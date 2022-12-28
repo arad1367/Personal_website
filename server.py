641bfd05fe59917bf69e8544c8035469cdc0c14c
arad1367 <pejman.ebrahimi77@gmail.com>
2022-12-28 10:34:42 +0100

    SQLAlchemy db is added

--- server.py
+++ server.py
@@ -2,45 +2,57 @@ from flask import Flask, render_template, request
 import smtplib
 from email.message import EmailMessage
 import os
-from pymongo import MongoClient
+from flask_sqlalchemy import SQLAlchemy
+# from pymongo import MongoClient
 
-application = Flask(__name__)
-app = application
+app = Flask(__name__)
 
-cluster_address = os.environ['CLUSTER']
-cluster = MongoClient(cluster_address)
-db = cluster['personalSiteDB']
-collection = db['customers']
+##Connect to Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///personalWeb.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-@application.route('/', methods=['GET','POST'])
+##Crypto TABLE Configuration
+class Web(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(250), unique=False, nullable=False)
+    name = db.Column(db.String(500), unique=False, nullable=False)
+    message = db.Column(db.String(500), unique=False, nullable=False)
+
+# Make database
+db.create_all()
+
+# cluster_address = os.environ['CLUSTER']
+# cluster = MongoClient(cluster_address)
+# db = cluster['personalSiteDB']
+# collection = db['customers']
+
+@app.route('/', methods=['GET','POST'])
 def home_page():
     if request.method == 'POST':
-        name = request.form['name']
-        email = request.form['email']
-        message = request.form['message']
+        new_user = Web(
+            name=request.form['name'],
+            email = request.form['email'],
+            message = request.form['message']
+                          )
 
-        customer = {
-            "Name": name,
-            "Email": email,
-            "Message": message
-        }
-
-        collection.insert_one(customer)
+        db.session.add(new_user)
+        db.session.commit()
 
         msg = EmailMessage()
-        msg.set_content(message)
+        msg.set_content(new_user.message)
         owner_email = os.environ['OWNEREMAIL']
         owner_pass = os.environ['OWNERPASS']
         msg['Subject'] = 'New message from Personal website'
-        msg['From'] = f"{email}"
+        msg['From'] = f'{new_user.email}'
         msg['To'] = owner_email
         # Send the message via our own SMTP server.
         server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
         server.login(owner_email, owner_pass)
         server.send_message(msg)
         server.quit()
-        return render_template('index.html', msg_sent=True, name=name)
+        return render_template('index.html', msg_sent=True, name=new_user.name)
     return render_template('index.html', msg_sent=False)
 
 if __name__ == '__main__':
-    application.run(debug=True)
+    app.run(debug=True)
